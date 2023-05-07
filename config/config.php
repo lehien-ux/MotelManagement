15ad0e08936206b4d2367899ef29b1eefca40a05
Hien Le <hienle25102001@gmail.com>
2023-05-07 20:47:28 +0700

    [Fix] update VNPAY sandbox for 2.1.0 version

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'vnp_TmnCode' => "HPTD9KTH", //Mã website tại VNPAY
-    'vnp_HashSecret' => "SWFEPTFRYPREJCKYWRWRPXVQWGWDTKZD", //Chuỗi bí mật
-    'vnp_Url' => "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html"
+    'vnp_TmnCode' => "BREZTAWY", //Mã website tại VNPAY
+    'vnp_HashSecret' => "VWSETPURJIUDHNRLJBUQSNMCHISESLSD", //Chuỗi bí mật
+    'vnp_Url' => "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"
 ];
